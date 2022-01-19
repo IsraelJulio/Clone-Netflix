a4d9108cc409144e240183cea2ef561db0d7cb49
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2022-01-19 01:15:26 -0300

    adicionado botão para efeito slider

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,26 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
 
 
-// const fi = () => { 
-//     const url = `http://www.omdbapi.com/?t=marvel`
-//     fetch(url)
-//         .then(response.json())
-//         .then(filme => {
-//             console.log(filme)
-//     })
-// }
+
